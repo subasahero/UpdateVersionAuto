b5bf2ad504790f310717808ff9276173312706dc
hieudt <hieudt@gmail.com>
2020-07-21 14:24:37 +0700

    finish create new

--- ToolUpdateInvoice/Form1.Designer.cs
+++ ToolUpdateInvoice/Form1.Designer.cs
@@ -28,22 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txPath = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.txNewDomain = new System.Windows.Forms.TextBox();
             this.txNewDataBase = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // txPath
-            // 
-            this.txPath.Location = new System.Drawing.Point(79, 42);
-            this.txPath.Name = "txPath";
-            this.txPath.Size = new System.Drawing.Size(204, 20);
-            this.txPath.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 148);
+            this.button1.Location = new System.Drawing.Point(134, 97);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -53,40 +47,60 @@
             // 
             // txNewDomain
             // 
-            this.txNewDomain.Location = new System.Drawing.Point(79, 78);
+            this.txNewDomain.Location = new System.Drawing.Point(116, 21);
             this.txNewDomain.Name = "txNewDomain";
             this.txNewDomain.Size = new System.Drawing.Size(204, 20);
             this.txNewDomain.TabIndex = 2;
             // 
             // txNewDataBase
             // 
-            this.txNewDataBase.Location = new System.Drawing.Point(79, 113);
+            this.txNewDataBase.Location = new System.Drawing.Point(116, 56);
             this.txNewDataBase.Name = "txNewDataBase";
             this.txNewDataBase.Size = new System.Drawing.Size(204, 20);
             this.txNewDataBase.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name Database";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name Domain";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 196);
+            this.ClientSize = new System.Drawing.Size(358, 143);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txNewDataBase);
             this.Controls.Add(this.txNewDomain);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.txPath);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Create Invoice";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txPath;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txNewDomain;
         private System.Windows.Forms.TextBox txNewDataBase;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ToolUpdateInvoice/Form1.cs
+++ ToolUpdateInvoice/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.IO.Compression;
@@ -22,36 +24,30 @@ namespace ToolUpdateInvoice
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string newDomain = txNewDomain.Text;
+                string newDataBase = txNewDataBase.Text;
+
+                string pathSampleFolder = ConfigurationManager.AppSettings.Get("pathFolderSample").ToString();
+                string pathDesFolder = ConfigurationManager.AppSettings.Get("pathFolder").ToString() + newDomain;
+                string pathUse = CopyFolder(pathSampleFolder, pathDesFolder);
+                bool replaceDomain = ReplaceInFile(pathUse + @"\wwwroot\env.js", "https://localhost:44383", "https://" + newDomain);
+
+                bool replaceDatabase = ReplaceInFile(pathUse + @"\appsettings.json", "<NameDatabase>", newDataBase);
+
+                string connectionString = ConfigurationManager.AppSettings.Get("connectString").ToString();
+                string diskBak = ConfigurationManager.AppSettings.Get("pathFileBak").ToString();
+                var a = CreateNewDatabase(connectionString, newDataBase, diskBak);
+                MessageBox.Show("Successfull");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro");
+                throw;
+            }
             
-            string path = txPath.Text;
-            string newDomain = txNewDomain.Text;
-            string newDataBase = txNewDataBase.Text;
-            System.IO.DirectoryInfo di = new DirectoryInfo(@"" + path);
 
-            foreach (FileInfo file in di.GetFiles())
-            {
-                string fileName = file.Name;
-                if (file.Name != "appsettings.json" && file.Name != "web.config")
-                {
-                    file.Delete();
-                }
-            }
-            foreach (DirectoryInfo dir in di.GetDirectories())
-            {
-                string folderName = dir.Name;
-                if(folderName != "Assets")
-                {
-                    dir.Delete(true);
-                }
-            }
-
-            ZipFile.ExtractToDirectory(@"D:\PublicForDemo\PublicForDemo.zip", path);
-
-            bool replaceDomain = ReplaceInFile(path + @"\wwwroot\env.js", "https://localhost:44383", "https://" + newDomain);
-
-            bool replaceDatabase = ReplaceInFile(path + @"\appsettings.json", "BaoAnInvoice", newDataBase);
-
-            MessageBox.Show("update success");
         }
 
         public bool ReplaceInFile(string filePath, string searchText, string replaceText)
@@ -66,14 +62,69 @@ namespace ToolUpdateInvoice
                 writer.Write(content);
                 writer.Close();
                 return true;
-
-
-
             }
             catch (Exception)
             {
                 return false;
             }
+        }
+
+        public string CopyFolder(string SourcePath, string DestinationPath)
+        {
+            try
+            {
+                //Now Create all of the directories
+                foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+                    SearchOption.AllDirectories))
+                    Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+                //Copy all the files & Replaces any files with the same name
+                foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+                    SearchOption.AllDirectories))
+                    File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+
+                return DestinationPath;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public bool CreateNewDatabase(string sqlConnection, string nameDatabase, string diskBak)
+        {
+            String str;
+            SqlConnection myConn = new SqlConnection(sqlConnection);
+            str = @"RESTORE FILELISTONLY FROM DISK='" + diskBak + "' " +
+                  @"RESTORE DATABASE " + nameDatabase + @" FROM DISK='" + diskBak + "' " +
+                "WITH " +
+                @"MOVE 'TrungNghiaInvoice' TO 'D:\Database\Invoice\" + nameDatabase + ".mdf'," +
+                @"MOVE 'TrungNghiaInvoice_log' TO 'D:\Database\Invoice\" + nameDatabase + ".ldf' ";
+
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
